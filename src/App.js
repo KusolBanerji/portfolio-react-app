9cc4bb1fcc4e0ba6b8b29fa6ffe90a5d96f9d90e
Kusol <kusol.banerji100@gmail.com>
2022-03-27 23:09:05 +0530

    Created Layout.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./Layout";
 
 function App() {
   return (
-    <h1>Hello World</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<h1>Home</h1>} />
+          <Route path="blogs" element={<h1>Blogs</h1>} />
+          <Route path="contact" element={<h1>Contact</h1>} />
+          <Route path="*" element={<h1>Error</h1>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
