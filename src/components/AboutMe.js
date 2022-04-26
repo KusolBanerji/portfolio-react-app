93f93d5b88c606f8be4258e1d76a02fa6252cd03
Kusol <kusol.banerji100@gmail.com>
2022-04-26 16:50:53 +0530

    Added about info in about page

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -10,11 +10,29 @@ const AboutMe = () => {
                     <img Style="width:40px;height:40px;float:right;" src={About} alt="about" />
                 </h1>
                 <hr />
-                <div Style="width:40%;">
-
-                </div>
                 <div>
-
+                    <div className="about-info" id="summary">
+                        <h2>Software Engineer</h2>
+                        <p>
+                            Strong in design and integration with intuitive problem-solving skills. 
+                            Proficient in JAVA, PYTHON, JAVASCRIPT, SQL and NOSQL. Passionate 
+                            about implementing and launching new projects. Ability to translate business 
+                            requirements into technical solutions.
+                        </p>
+                    </div>
+                    <div className="about-info" id="personal-info">
+                        <h2>Personal Information</h2>
+                        <p>
+                            <b><u>Name</u> :</b> Kusol Banerji<br/>
+                            <b><u>Age</u> :</b> 22 Years<br/>
+                            <b><u>Residence</u> :</b> Kolkata, India<br/>
+                            <b><u>Email</u> :</b> kusol.banerji100@gmail.com<br/>
+                            <b><u>Phone</u> :</b> (+91) 700 301 1271<br/>
+                        </p>
+                    </div>
+                </div>
+                <div className="services">
+                    <h2>Services</h2>
                 </div>
             </div>
         </div>
